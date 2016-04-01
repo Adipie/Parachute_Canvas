cd5f916438660cd373d3470d76444379668f1abb
adi <adipiek@gmail.com>
2016-04-01 18:43:17 +0300

    added drop parachuters

--- game.js
+++ game.js
@@ -12,6 +12,8 @@ window.onload = function() {
 		boatPosition,
 		plane,
 		planePosition,
+		planeInterval,
+		dropTimer,
 		score,
 		scoreString,
 		lives,
@@ -45,25 +47,33 @@ window.onload = function() {
 		score = 0;
 		ctx.fillText(scoreString + score, 40,30);
 
-		lifeString = 'lives: ';
+		lifeString = 'Lives: ';
 		lives = 3;
 		ctx.fillText(lifeString + lives, 40, 70);
 
 		ctx.fillText('- Hit space bar to start the game -', 450, 300);
-		window.addEventListener('keydown',startGame,true);
+		window.addEventListener('keydown',startGame,false);
 	}
 
 	//start game
 	function startGame(key){
 		if (key.keyCode === 32){
+			score = 0;
+			lives = 3;
 			ctx.clearRect(450,200,500,200);
+			// plane starts moving
 			startPlaneAnimation();
+			// catch keyboard events to move the boat
 			window.addEventListener('keydown',moveBoat,true);
+			// drop parachuters at random interval
+			dropTimer = Math.round(Math.random() * 5000) + 500;
+			var dropInterval = setInterval(dropParachuters,dropTimer); 
 		}
 	}
 
+	//  start plane animations
 	function startPlaneAnimation(){
-		setInterval(function(){
+		planeInterval = setInterval(function(){
 			ctx.clearRect(planePosition,100,plane.width,plane.height);
 			if (planePosition < -100){
 				planePosition = 1200;
@@ -75,19 +85,20 @@ window.onload = function() {
 		} ,30);
 	}
 
+	// move boat using keyboards
 	function moveBoat(key){
 		switch(key.keyCode){
 			case (37): 
 			if (boatPosition > 0){
 				ctx.clearRect(boatPosition, 400, boat.width, boat.height);
-				boatPosition -= 10;
+				boatPosition -= 20;
 				ctx.drawImage(boat,boatPosition, 400);
 			}
 			break;
 			case (39):
 			if (boatPosition < 1150){
 				ctx.clearRect(boatPosition, 400, boat.width, boat.height);
-				boatPosition += 10;
+				boatPosition += 20;
 				ctx.drawImage(boat,boatPosition, 400);
 			}
 			break;
@@ -95,8 +106,51 @@ window.onload = function() {
 		}
 	}
 
-	//ctx.clearRect(40,0, 350,50);
-	
+	// drop parachuters 
+	function dropParachuters(){
+		if (planePosition > 10 && planePosition < 1150){
+			var parachute = new Image();
+			parachute.src = 'assets/parachute.png';
+			var parachuteXPosition = planePosition + 50;
+			var parachuteYPosition = 140;
+			parachute.onload = function(){				
+			ctx.drawImage(parachute, parachuteXPosition ,parachuteYPosition);	
+			}
+			setInterval(function(){			
+				ctx.clearRect(parachuteXPosition,parachuteYPosition,parachute.width,parachute.height);
+				parachuteYPosition += 10;
+				ctx.drawImage(parachute, parachuteXPosition ,parachuteYPosition);
+				if (parachuteYPosition > (400 + boat.height)) {
+					if (parachuteXPosition > boatPosition && parachuteXPosition < (boatPosition + boat.width)){
+						score += 10;
+						ctx.clearRect(40,0, 350,50);
+						ctx.fillText(scoreString + score, 40,30);
+						delete parachute();
+					}
+				}
+			},30);	
+		}	
+	}
+
+	function handleParachuteLanding(parachute,parachuteXPosition,parachuteYPosition){
+		if (parachuteYPosition > (400 + boat.height)) {
+			console.log(parachuteXPosition);
+			console.log(parachuteYPosition);
+			if (parachuteXPosition > boatPosition && parachuteXPosition < (boatPosition + boat.width)){
+				score += 10;
+				ctx.clearRect(40,0, 350,50);
+				ctx.fillText(scoreString + score, 40,30);
+			}
+		}
+	}
+	/*if (parachuteXPosition > boatPosition && parachuteXPosition < boatPosition+boat.width && parachuteYPosition === 400+boat.height){
+				score += 10;
+				ctx.clearRect(40,0, 350,50);
+				ctx.fillText(scoreString + score, 40,30);
+				clearInterval(parachuteInterval);				
+			}
+
+			*/
 	//handle parachute-boat collision
 	//handle missed parachute
 	//game over
